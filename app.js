9bcfcc8ab1a42f1cd890a80975d9ca323ef39c7a
Lucas <lucassaox@gmail.com>
2025-02-10 15:54:46 -0300

    refactor: remove alinhamentos excessivos para melhorar legibilidade

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ let ultimoAmigoSoteardo;
 
 function adicionarAmigo(){
     let nome = document.getElementById('amigo').value;
-    if(nome.trim() == ""){
-        alert("Digite o nome de um amigo!")
-    }else {
-        if (amigoEstaNaLista(nome)){
-            alert("Esse amigo já está na lista!")
-        }else{
-            let novoItem = document.createElement("li");
-            novoItem.textContent = nome;
-            listaAmigos.appendChild(novoItem)
-        }
-    }
+    if(nome.trim() == "") return alert("Digite o nome de um amigo!");
+  
+    if (amigoEstaNaLista(nome)) return alert("Esse amigo já está na lista!");
+        
+    let novoItem = document.createElement("li");
+    novoItem.textContent = nome;
+    listaAmigos.appendChild(novoItem)
 }
 
 function amigoEstaNaLista(amigo){
@@ -34,38 +30,31 @@ function amigoEstaNaLista(amigo){
 
 function sortearAmigo(){
     let quantidadeDePessoas = listaAmigos.children.length;
-    if (quantidadeDePessoas == 0){
-        alert("Antes de fazer o sorteio adicione os nomes");
+    if (quantidadeDePessoas == 0) return alert("Antes de fazer o sorteio adicione os nomes");
 
-    } else if (quantidadeDePessoas == listaDeAmigosEscolhidos.length){
+    if (quantidadeDePessoas == listaDeAmigosEscolhidos.length){
         mostrarResultado("Todos os nomes já foram sorteados, adicione mais nomes ou recomece o sorteio")
+    } 
 
-    } else {
-        let indiceAleatorio = parseInt(Math.random() * quantidadeDePessoas);
-        
-        amigoAleatorio = listaAmigos.children[indiceAleatorio].textContent;
-        
-        if (listaDeAmigosEscolhidos.includes(amigoAleatorio)){
-            sortearAmigo();
-        }else{
-            listaDeAmigosEscolhidos.push(amigoAleatorio);
-            pilhaAmigosSorteados.push(amigoAleatorio);
-            ultimoAmigoSoteardo = amigoAleatorio;
-            mostrarResultado (`O amigo secreto sorteado é :${ultimoAmigoSoteardo}`);
-            revelarBtnEuSou();
-        }
-    }
+    let indiceAleatorio = parseInt(Math.random() * quantidadeDePessoas);
+    amigoAleatorio = listaAmigos.children[indiceAleatorio].textContent;
+
+    if (listaDeAmigosEscolhidos.includes(amigoAleatorio)) return sortearAmigo();
+
+    listaDeAmigosEscolhidos.push(amigoAleatorio);
+    pilhaAmigosSorteados.push(amigoAleatorio);
+    ultimoAmigoSoteardo = amigoAleatorio;
+    mostrarResultado (`O amigo secreto sorteado é :${ultimoAmigoSoteardo}`);
+    revelarBtnEuSou();
 }
 
 function confirmarMeuNome() {
     let resposta = confirm("Este é o seu nome? Se sim, clique em OK para desfazer o sorteio e sortear novamente. Se não, clique em Cancelar para continuar.");
-    if (resposta) {
-        desfazerUltimoSorteio();
-    } else {
-        alert("Obrigado por confirmar! Por favor, passe o dispositivo para a próxima pessoa.");
-        esconderBtnEuSou();
-        resultado.innerHTML = "";
-    }
+    if (resposta)  return desfazerUltimoSorteio();
+
+    alert("Obrigado por confirmar! Por favor, passe o dispositivo para a próxima pessoa.");
+    esconderBtnEuSou();
+    resultado.innerHTML = "";
 }
 
 function desfazerUltimoSorteio(){
@@ -85,6 +74,7 @@ function revelarBtnEuSou(){
     let btnEuSou = document.getElementById('btnEuSou');
     if (btnEuSou.classList.contains('hidden-button')){
         btnEuSou.classList.replace('hidden-button', 'button-draw');
+        btnEuSou.textContent = `Epa! Eu sou ${ultimoAmigoSoteardo}`;
     }
 }
 
